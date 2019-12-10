f60501b94484258d2769ac0c36e3198d4a1c204c
T10000595@live.yuntech.edu.tw <T10000595@live.yuntech.edu.tw>
2019-12-11 00:09:49 +0800

    1.請修改 Course, Department, Person 表格，新增 DateModified 欄位(datetime)，並且這三個表格的資料透過 Web API 更新時，都要自動更新該欄位為更新當下的時間
    2.請修改 Course, Department, Person 表格欄位，新增 IsDeleted 欄位 (bit)，且讓所有刪除這三個表格資料的 API 都不能真的刪除資料，而是標記刪除即可，標記刪除後，在 GET 資料的時候不能輸出該筆資料。

--- Contosouniversity/Models/ContosouniversityContext_SaveChanges.cs
+++ Contosouniversity/Models/ContosouniversityContext_SaveChanges.cs
@@ -12,23 +12,23 @@ namespace MvcWithEf.Models
 
         public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
         {
-            ChangeStates();
+            FilterBeforeModified();
             return base.SaveChangesAsync(acceptAllChangesOnSuccess,cancellationToken);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
-            ChangeStates();
+            FilterBeforeModified();
             return base.SaveChangesAsync(cancellationToken);
         }
 
         public override int SaveChanges()
         {
-            ChangeStates();
+            FilterBeforeModified();
             return base.SaveChanges();
         }
 
-        private void ChangeStates()
+        private void FilterBeforeModified()
         {
             var entities = this.ChangeTracker.Entries();
             foreach (var entry in entities)
@@ -44,12 +44,11 @@ namespace MvcWithEf.Models
                 else if (entry.State == EntityState.Deleted)
                 {
                     //請修改 Course, Department, Person 表格欄位，新增 IsDeleted 欄位 (bit)，且讓所有刪除這三個表格資料的 API 都不能真的刪除資料，而是標記刪除即可，標記刪除後，在 GET 資料的時候不能輸出該筆資料。
-                    string EntityTypeName = entry.Entity.GetType().Name;
-                    switch (EntityTypeName)
+                    switch (entry.Entity.GetType().Name)
                     {
-                        case "Course":
-                        case "Department":
-                        case "Person":
+                        case nameof(Course):
+                        case nameof(Department):
+                        case nameof(Person):
                             entry.State = EntityState.Modified;
                             entry.CurrentValues.SetValues(
                                 new { IsDeleted = true }
